bcf26da50c3b564f8d1398d2311509390a50dd8b
Andrew-Baher <37151807+Andrew-Baher@users.noreply.github.com>
2018-03-09 22:25:23 +0200

    Course work Layout only
    added a course work view for the project without and logic or coneecting ot the server to get the data from

---
+++ CUFE/app/src/main/java/com/example/ahmed/cufe/Course_work.java
@@ -0,0 +1,19 @@
+package com.example.ahmed.cufe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.content.Intent;
+
+
+
+
+public class Course_work extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_work);
+    }
+}
